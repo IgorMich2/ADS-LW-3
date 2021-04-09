35738cfac33ed4625ac414595e790011ba821811
IgorMich2 <70755229+IgorMich2@users.noreply.github.com>
2021-04-09 20:12:46 +0300

    Updated demo

--- LW 3/LW 3/Source.cpp
+++ LW 3/LW 3/Source.cpp
@@ -10,48 +10,47 @@ template <class T>
 class Heap
 {
 	vector <T> arr;
-	void reHeapUp(int);
-	void reHeapUpMin(int);
-	void reHeapDown(int);
-	int getLeftChild(int) const;
-	int getRightChild(int) const;
-	int getParent(int) const;
+	void reHeapUp(int child);
+	void reHeapUpMin(int child);
+	void reHeapDown(int parent);
+	int getLeftChild(int parent);
+	int getRightChild(int parent);
+	int getParent(int child);
 
 public:
 	Heap() {}
-	void insertKey(T);
+	void insertKey(T date);
 	T deleteKey();
-	int getHeapSize() const;
-	void display() const;
+	int getHeapSize();
+	void display();
 	void maxheap();
 	void minheap();
 	bool isempty();
-	bool isfull();
 	void Find_k_min_1(int k);
 	void Find_k_min_2(int k);
 	void Find_k_min_3(int k);
 };
 
 template <class T>
-int Heap<T> ::getHeapSize() const
+int Heap<T> ::getHeapSize()
 {
 	return arr.size();
 }
 
 template <class T>
-int Heap<T> ::getLeftChild(int parent) const
+int Heap<T> ::getLeftChild(int parent)
 {
 	return 2 * parent + 1;
 }
 
 template <class T>
-int Heap<T> ::getRightChild(int parent) const
+int Heap<T> ::getRightChild(int parent)
 {
 	return 2 * parent + 2;
 }
 
 template <class T>
-int Heap<T> ::getParent(int child) const
+int Heap<T> ::getParent(int child)
 {
 	return (child - 1) / 2;
 }
@@ -59,14 +58,10 @@ int Heap<T> ::getParent(int child) const
 template <class T>
 void Heap<T> ::reHeapUp(int child)//maxheap
 {
-	// if child is not root then continue
 	if (child != 0)
 	{
-		// get parent of the child
 		int curr_parent = getParent(child);
-		//if data at child index is more than at
-		//parent index then swap and continue again
-		//till the condition gets false
+
 		if (arr[child] > arr[curr_parent])
 		{
 			swap(arr[child], arr[curr_parent]);
@@ -78,14 +73,10 @@ void Heap<T> ::reHeapUp(int child)//maxheap
 template <class T>
 void Heap<T> ::reHeapUpMin(int child)//minheap
 {
-	// if child is not root then continue
 	if (child != 0)
 	{
-		// get parent of the child
 		int curr_parent = getParent(child);
-		//if data at child index is more than at
-		//parent index then swap and continue again
-		//till the condition gets false
+
 		if (arr[child] < arr[curr_parent])
 		{
 			swap(arr[child], arr[curr_parent]);
@@ -100,23 +91,17 @@ void Heap<T> ::reHeapDown(int parent)
 
 	int left = getLeftChild(parent);
 
-	// if there exists a left subtree
 	if (left < arr.size())
 	{
-		//cout<<"left: "<<left<<endl;
 		int right = getRightChild(parent);
 
-		// if there exists a right subtree
 		if (right < arr.size())
 		{
-			//cout<<"right: "<<right<<endl;
 			if (arr[left] < arr[right])
 			{
 				if (arr[parent] < arr[right])
 				{
 					swap(arr[parent], arr[right]);
-					/*cout<<"in left < right: "<<endl;
-					display()*/;
 					parent = right;
 					reHeapDown(parent);
 				}
@@ -126,21 +111,17 @@ void Heap<T> ::reHeapDown(int parent)
 				if (arr[parent] < arr[left])
 				{
 					swap(arr[parent], arr[left]);
-					/*cout<<"in left > right: "<<endl;
-					display();*/
+
 					parent = left;
 					reHeapDown(parent);
 				}
 			}
 		}
-		// if no right subtree
 		else
 		{
 			if (arr[parent] < arr[left])
 			{
 				swap(arr[parent], arr[left]);
-				/*cout<<"No right subtree: "<<endl;
-				display();	*/
 			}
 		}
 
@@ -150,18 +131,14 @@ void Heap<T> ::reHeapDown(int parent)
 template <class T>
 void Heap<T> ::insertKey(T data)
 {
-	// data is always added at the leaf
 	arr.push_back(data);
 	int child = arr.size() - 1;
 	reHeapUp(child);
-	//reHeapUpMin(child);
 }
 
 template <class T>
 T Heap<T> ::deleteKey()
 {
-	// For Heaps delete function is used to return
-	// max value from the heap
 	if (arr.size() == 0)
 	{
 		cout << "Heap empty...returning -1";
@@ -169,20 +146,17 @@ T Heap<T> ::deleteKey()
 	}
 
 	int child = arr.size() - 1;
-	// swap root value with last element
 	swap(arr[0], arr[child]);
 
-	// since root is swapped with last element it is
-	// max and get that element
 	T max_ele = arr.back();
-	// dec the size of array
+
 	arr.pop_back();
 	reHeapDown(0);
 	return max_ele;
 }
 
 template <class T>
-void Heap<T> ::display() const
+void Heap<T> ::display()
 {
 	if (arr.size() == 0)
 	{
@@ -206,11 +180,6 @@ void Heap<T> ::minheap()
 	sort(arr.begin(), arr.end());
 }
 
-template <class T>
-bool Heap<T> ::isfull()
-{
-	return false;
-}
 
 template <class T>
 bool Heap<T> ::isempty()
@@ -229,7 +198,7 @@ void Heap<T>::Find_k_min_1(int k) {
 		for (j = 0; j < len - i - 1; j++)
 			if (arr[j] > arr[j + 1])
 				swap(arr[j], arr[j + 1]);
-	cout << "k min: " << endl;
+	cout << "k min 1: " << endl;
 	for (int i = 0; i < k; i++) {
 		cout << arr[i] << " ";
 	}
@@ -239,7 +208,7 @@ void Heap<T>::Find_k_min_1(int k) {
 template <class T>
 void Heap<T>::Find_k_min_2(int k) {
 	minheap();
-	cout << "k min: " << endl;
+	cout << "k min 2: " << endl;
 	int l = arr.size();
 	int n = min(k, l);
 	for (int i = 0; i < n; i++) {
@@ -251,7 +220,7 @@ void Heap<T>::Find_k_min_2(int k) {
 template <class T>
 void Heap<T>::Find_k_min_3(int k) {
 	maxheap();
-	cout << "k min: " << endl;
+	cout << "k min 3: " << endl;
 	int len = arr.size();
 	for (int i = len - 1; i > len - k - 1; i--) {
 		cout << arr[i] << " ";
@@ -262,14 +231,21 @@ void Heap<T>::Find_k_min_3(int k) {
 int main()
 {
 	Heap<int> h;
-	int k = 7;
-	int a[] = { 9,10,7,6,23,8,16 };
 
-	for (int i = 0; i < 7; i++)
-		h.insertKey(a[i]);
+
+	/*h.insertKey(9);
+	h.insertKey(10);
+	h.insertKey(7);
+	h.insertKey(6);
+	h.insertKey(23);
+	h.insertKey(8);
+	h.insertKey(16);
+	*/
+	for (int i = 10; i >= 1; i--) {
+		h.insertKey(i);
+	}
+	cout << "Heap is: " << endl;
 	h.display();
-
-	cout << "Current heap is: " << endl;
 
 	cout << "Maxheap: " << endl;
 	h.maxheap();
@@ -278,10 +254,28 @@ int main()
 	cout << "Minheap: " << endl;
 	h.minheap();
 	h.display();
-
+	int t1 = clock();
 	h.Find_k_min_1(2);
+	int t2 = clock();
+	cout <<"Time is "<< t2-t1 <<" miliseconds"<< endl;
+	t1 = clock();
 	h.Find_k_min_2(2);
+	t2 = clock();
+	cout << "Time is " << t2 - t1 << " miliseconds" << endl;
+	t1 = clock();
 	h.Find_k_min_3(2);
+	t2 = clock();
+	cout << "Time is " << t2 - t1 << " miliseconds" << endl;
+
+
+	Heap<string> h2;
+	h2.insertKey("a");
+	h2.insertKey("b");
+	h2.insertKey("c");
+	h2.insertKey("d");
+	h2.insertKey("e");
+	h2.display();
+	cout<<h2.isempty();
 
 	return 0;
 }
